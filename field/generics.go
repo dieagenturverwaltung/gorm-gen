51002711d686e1243d6943b4642fe99e0e2346b2
MarlinKuhn <marlin.kuhn@web.de>
2026-03-03 16:16:56 +0100

    feat: migrate field methods to genericsField for improved type safety

--- field/generics.go
+++ field/generics.go
@@ -90,3 +90,73 @@ func (field genericsField[T]) toSlice(values ...T) []interface{} {
 	}
 	return slice
 }
+
+// AsString returns a String field
+func (field genericsField[T]) AsString() String {
+	return newChars[string](field.expr)
+}
+
+// AsBytes returns a Bytes field
+func (field genericsField[T]) AsBytes() Bytes {
+	return newChars[[]byte](field.expr)
+}
+
+// AsInt returns an Int field
+func (field genericsField[T]) AsInt() Int {
+	return newNumber[int](field.expr)
+}
+
+// AsInt8 returns an Int8 field
+func (field genericsField[T]) AsInt8() Int8 {
+	return newNumber[int8](field.expr)
+}
+
+// AsInt16 returns an Int16 field
+func (field genericsField[T]) AsInt16() Int16 {
+	return newNumber[int16](field.expr)
+}
+
+// AsInt32 returns an Int32 field
+func (field genericsField[T]) AsInt32() Int32 {
+	return newNumber[int32](field.expr)
+}
+
+// AsInt64 returns an Int64 field
+func (field genericsField[T]) AsInt64() Int64 {
+	return newNumber[int64](field.expr)
+}
+
+// AsUint returns a Uint field
+func (field genericsField[T]) AsUint() Uint {
+	return newNumber[uint](field.expr)
+}
+
+// AsUint8 returns a Uint8 field
+func (field genericsField[T]) AsUint8() Uint8 {
+	return newNumber[uint8](field.expr)
+}
+
+// AsUint16 returns a Uint16 field
+func (field genericsField[T]) AsUint16() Uint16 {
+	return newNumber[uint16](field.expr)
+}
+
+// AsUint32 returns a Uint32 field
+func (field genericsField[T]) AsUint32() Uint32 {
+	return newNumber[uint32](field.expr)
+}
+
+// AsUint64 returns a Uint64 field
+func (field genericsField[T]) AsUint64() Uint64 {
+	return newNumber[uint64](field.expr)
+}
+
+// AsFloat32 returns a Float32 field
+func (field genericsField[T]) AsFloat32() Float32 {
+	return newNumber[float32](field.expr)
+}
+
+// AsFloat64 returns a Float64 field
+func (field genericsField[T]) AsFloat64() Float64 {
+	return newNumber[float64](field.expr)
+}
